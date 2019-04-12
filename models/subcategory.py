56de260ad08fa35e75030693ca086cdcf7e69320
jnsy <jadensy99@gmail.com>
2019-04-12 11:51:01 +0800

    add models for db

--- models/subcategory.py
+++
@@ -1,7 +0,0 @@
-from models.base_model import BaseModel
-import peewee as pw
-
-
-class Subcategory(BaseModel):
-    category = pw.BooleanField() # safe = True, dangerous = False
-    subcategory_name = pw.CharField()
