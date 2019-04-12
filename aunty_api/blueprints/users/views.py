f3644bccd07a2e82e6610aa2f19e334cf5517f52
kharissa <kharissafernando@gmail.com>
2019-04-12 18:59:01 +0800

    Remove flask login

--- aunty_api/blueprints/users/views.py
+++ aunty_api/blueprints/users/views.py
@@ -1,48 +1,45 @@
+import jwt
+from models.user import User
 from flask import Blueprint, jsonify, request
 from werkzeug.security import generate_password_hash
-from models.user import User
-from flask_login import LoginManager, login_required, current_user, login_user
+from helpers import encode_auth_token, decode_auth_token
 
-users_api_blueprint = Blueprint('users_api',
-                             __name__,
-                             template_folder='templates')
 
-@users_api_blueprint.route('/', methods=['GET', 'POST'])
+users_api_blueprint = Blueprint('users_api', __name__, template_folder='templates')
+
+@users_api_blueprint.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        req_data = request.get_json()
-        first_name = req_data['firstName']
-        last_name = req_data['lastName']
-        email = req_data['email']
-        password = req_data['password']
-        date_of_birth = req_data['dateOfBirth']
-        nationality = req_data['nationality']
-        hashed_password = generate_password_hash(password)
-        
-        u = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password, date_of_birth=date_of_birth, nationality=nationality)
+    req_data = request.get_json()
+    first_name = req_data['firstName']
+    last_name = req_data['lastName']
+    email = req_data['email']
+    password = req_data['password']
+    date_of_birth = req_data['dateOfBirth']
+    nationality = req_data['nationality']
+    hashed_password = generate_password_hash(password)
+    
+    # Creating new user
+    u = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password, dob=date_of_birth, nationality=nationality)
 
-        if u.save():
-            return jsonify({
-                'auth_token': 'DUMMY_TOKEN',
-                'message': 'Successfully created a user and signed in.',
-                'status': 'success',
-                'user': {
-                    'id': 'DUMMY_USER_ID',
-                    'first_name': first_name,
-                    'last_name': last_name,
-                    'email': email,
-                    'date_of_birth': date_of_birth,
-                    'nationality': nationality
-                }
-            })
-        else:
-            errors = u.errors
-            return jsonify({
-                'status': 'failed',
-                'message': errors
-            })
-    else:
+
+    if u.save():  # If user instance saved, log in user and return token 
+        token = encode_auth_token(u)
+        return jsonify({
+            'auth_token': token,
+            'message': 'Successfully created the account. Please log in.',
+            'status': 'success',
+            'user': {
+                'id': u.id,
+                'first_name': u.first_name,
+                'last_name': u.last_name,
+                'email': u.email,
+                'date_of_birth': u.date_of_birth,
+                'nationality': u.nationality
+            }
+        })
+    else: # If user instance is unable to save, return errors
+        errors = u.errors
         return jsonify({
             'status': 'failed',
-            'message': 'Try a POST request!'
+            'message': errors
         })
--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
 from models.base_model import BaseModel
 import peewee as pw
-from flask_login import UserMixin
 from peewee_validates import ModelValidator, StringField, validate_email
 
-class User(BaseModel, UserMixin):
+class User(BaseModel):
     email = pw.CharField(unique=True)
     password = pw.CharField()
     first_name = pw.CharField()
@@ -21,17 +20,17 @@ class User(BaseModel, UserMixin):
         # self.errors.update(validator.errors)
         # AttributeError: 'User' object has no attribute 'errors'
 
-    def save(self, *args, **kwargs):
-        # Ensure unique email and username
-        validator = ModelValidator(self)
-        validator.validate()
-        self.errors.update(validator.errors)
+    # def save(self, *args, **kwargs):
+    #     # Ensure unique email and username
+    #     validator = ModelValidator(self)
+    #     validator.validate()
+    #     self.errors.update(validator.errors)
 
-        if self.errors:
-            return 0
-        else:
-            self.updated_at = datetime.datetime.now()
-            return super(BaseModel, self).save(*args, **kwargs)
+    #     if self.errors:
+    #         return 0
+    #     else:
+    #         self.updated_at = datetime.datetime.now()
+    #         return super(BaseModel, self).save(*args, **kwargs)
 
     class CustomValidator(ModelValidator):
         email = StringField(required=True, validators=[validate_email()])
