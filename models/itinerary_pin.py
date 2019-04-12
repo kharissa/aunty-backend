8467dab430180ef823d4177518be9964231d505c
kharissa <kharissafernando@gmail.com>
2019-04-12 16:57:53 +0800

    Updated models with long/lat params

--- models/itinerary_pin.py
+++ models/itinerary_pin.py
@@ -4,8 +4,8 @@ import peewee as pw
 
 class ItineraryPin(BaseModel):
     user = pw.ForeignKeyField(User, backref="itinerary")
-    # longitude = pw.FloatField(10,6)
-    # latitude = pw.FloatField(10,6)
+    longitude = pw.DecimalField(max_digits=9, decimal_places=6)
+    latitude = pw.DecimalField(max_digits=9, decimal_places=6)
     name = pw.CharField(max_length=255)
     description = pw.CharField(max_length=1000, null=True)
     address = pw.CharField(null=True)
