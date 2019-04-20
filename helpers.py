97ac5730cb91623f739d663ac751d5c42079e370
nyx-ell <nicole.linyx@gmail.com>
2019-04-20 17:01:01 +0800

    Added AWS config

--- helpers.py
+++ helpers.py
@@ -1,6 +1,14 @@
 import jwt
 import datetime
 from app import app
+import boto3, botocore
+from config import S3_KEY, S3_SECRET, S3_BUCKET
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=S3_KEY,
+   aws_secret_access_key=S3_SECRET
+)
 
 def encode_auth_token(self):
     try:
