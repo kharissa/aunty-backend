0ed82a5ef5d32036b952c9b866d043b0841dc7ff
kharissa <kharissafernando@gmail.com>
2019-04-20 18:31:32 +0800

    Call Sightengine API with received file data

--- aunty_api/blueprints/images/views.py
+++ aunty_api/blueprints/images/views.py
@@ -1,18 +1,78 @@
+from app import app
 from models.user import User
 from models.image import Image
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, redirect, url_for
 from helpers import decode_auth_token
 from sightengine.client import SightengineClient
 
 images_api_blueprint = Blueprint('images_api', __name__, template_folder='templates')
 
-# @images_api_blueprint.route('/', methods=['POST'])
-# def create():
-#     pass
+@images_api_blueprint.route('/', methods=['POST'])
+def create():
+    auth_header = request.headers.get('Authorization')
 
-@images_api_blueprint.route('/')
-def update():
-    # image = Image.get(Image.id == image_id)
-    client = SightengineClient('112720407', 'SY6rzumjz42j4PG2nKGw')
-    output = client.check('nudity','wad','offensive','faces','scam','face-attributes').set_url('https://d3m9459r9kwism.cloudfront.net/img/examples/example7.jpg')
-    return output
+    if auth_header:
+        token = auth_header.split(" ")[1]
+    else:
+        return jsonify([{
+            'status': 'failed',
+            'message': 'Not authorization header.'
+        }])
+
+    decoded = decode_auth_token(token)
+    user = User.get(User.id == decoded)
+
+    if user:
+        ##########################
+        # Upload Image to AWS S3 #
+        ##########################
+        filename = 'kharissa.jpeg' # Dummy data until we receive filename from
+        image = Image(filename=filename, user=user)
+
+        if image.save():
+            image_url = 'https://d3m9459r9kwism.cloudfront.net/img/examples/example7.jpg'  # image.url
+            client = SightengineClient(app.config.get(
+                'SIGHTENGINE_USER'), app.config.get('SIGHTENGINE_SECRET'))
+            output = client.check('nudity','wad','offensive', 'scam','face-attributes').set_url(image_url)
+            image.weapon = output['weapon']
+            image.alcohol = output['alcohol']
+            image.drugs = output['drugs']
+            image.male = output['faces'][0]['attributes']['male']
+            image.female = output['faces'][0]['attributes']['female']
+            image.minor = output['faces'][0]['attributes']['minor']
+            image.sunglasses = output['faces'][0]['attributes']['sunglasses']
+            image.scam = output['scam']['prob']
+            image.nudity = output['nudity']['safe']
+
+            if image.save():
+                return jsonify({
+                    'status': 'success',
+                    'message': 'Image analyzed successfully.',
+                    'results': {
+                        'weapon': image.weapon,
+                        'alcohol': image.alcohol,
+                        'drugs': image.drugs,
+                        'male': image.male,
+                        'female': image.female,
+                        'minor': image.minor,
+                        'sunglasses': image.sunglasses,
+                        'scam': image.scam,
+                        'nudity': image.nudity
+                    }
+                })
+            else:
+                return jsonify({
+                    'status': 'failed',
+                    'message': 'Something went wrong. The image was unable to be analyzed.'
+                })
+        else:
+            return jsonify({
+                'status': 'failed',
+                'message': 'Image was unsuccessfully uploaded.'
+            })
+    else:
+        return jsonify({
+            'status': 'failed',
+            'message': 'No user found.'
+        })
+
