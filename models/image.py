a562bc0b526f91dc2ddf5fad6bb62add30a15024
nyx-ell <nicole.linyx@gmail.com>
2019-04-20 17:00:47 +0800

    Added image model

---
+++ models/image.py
@@ -0,0 +1,16 @@
+from models.base_model import BaseModel
+from models.user import User
+import peewee as pw
+
+class Image(BaseModel):
+    user = pw.ForeignKeyField(User, backref="images")
+    filename = pw.CharField()
+    weapon = pw.FloatField(null=True)
+    alcohol = pw.FloatField(null=True)
+    drugs = pw.FloatField(null=True)
+    male = pw.FloatField(null=True)
+    female = pw.FloatField(null=True)
+    minor = pw.FloatField(null=True)
+    sunglasses = pw.FloatField(null=True)
+    scam = pw.FloatField(null=True)
+    nudity = pw.FloatField(null=True)
