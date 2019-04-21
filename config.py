32410fadc4980f9acdd71506b686c6d0de06841d
nyx-ell <nicole.linyx@gmail.com>
2019-04-22 00:59:25 +0800

    Updated s3 location

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 S3_BUCKET                 = os.environ.get("S3_BUCKET")
 S3_KEY                    = os.environ.get("S3_KEY")
 S3_SECRET                 = os.environ.get("S3_SECRET")
-# S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 S3_LOCATION               = 'https://s3.amazonaws.com/{}/'.format(S3_BUCKET)
 
 class Config(object):
