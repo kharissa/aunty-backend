da25d3be43a64f9c790edab8ce93aa4942ec3e3f
nyx-ell <nicole.linyx@gmail.com>
2019-04-23 20:28:28 +0800

    Latest front end build files

---
+++ aunty_web/static/build/precache-manifest.b2266714650019238e609695fe085a26.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c15d7bef615fe66d1fe",
+    "url": "/static/css/main.3e31a020.chunk.css"
+  },
+  {
+    "revision": "7c15d7bef615fe66d1fe",
+    "url": "/static/js/main.e0405097.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f190d4e7e7ced4bc1278",
+    "url": "/static/css/2.3ee777de.chunk.css"
+  },
+  {
+    "revision": "f190d4e7e7ced4bc1278",
+    "url": "/static/js/2.587921ab.chunk.js"
+  },
+  {
+    "revision": "f30ef74b314ec6759265158e0f30b1ce",
+    "url": "/static/media/aunty.f30ef74b.png"
+  },
+  {
+    "revision": "6ce6c461d288a09a5070a5ea9692d322",
+    "url": "/static/media/aunty_hello.6ce6c461.jpeg"
+  },
+  {
+    "revision": "8efe6f0e4b861012672c188bb9443081",
+    "url": "/static/media/sos.8efe6f0e.jpg"
+  },
+  {
+    "revision": "07e475b96f106b1e497c7d9865151d47",
+    "url": "/static/media/old_phone.07e475b9.mp3"
+  },
+  {
+    "revision": "f303cf0c3d3fd8c1742fa908393231b4",
+    "url": "/static/media/phonecall.f303cf0c.mpeg"
+  },
+  {
+    "revision": "30129b7240c6dbce97155a0eab85528c",
+    "url": "/index.html"
+  }
+];
