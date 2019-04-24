5dcce9d42ac301a407dad0be3857a6effe082272
nyx-ell <nicole.linyx@gmail.com>
2019-04-24 14:20:36 +0800

    Updated onesignal config

--- aunty_web/static/js/onesignal.js
+++ aunty_web/static/js/onesignal.js
@@ -1,11 +1,7 @@
 var OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
     OneSignal.init({
-        appId: "d76693f6-d95e-4cca-93c1-b875207242e9",
-        autoResubscribe: true,
-        notifyButton: {
-            enable: true,
-        },
+        appId: "d76693f6-d95e-4cca-93c1-b875207242e9"
     });
     OneSignal.showNativePrompt();
     OneSignal.sendTags({
